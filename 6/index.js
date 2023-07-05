6f86d29ad2fc3935be7625aa0b971737cf76098e
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-07-05 07:59:51 -0500

    Se mejora el procedimiento

--- 6/index.js
+++ 6/index.js
@@ -5,7 +5,7 @@
     • Si la Temperatura es más de 26°C  o 78°F : Tiene fiebre o es el apocalipsis. */
 
 let numero = parseFloat(prompt("Ingrese los grados a calcular"));
-let grados = parseInt(prompt("Si desea hacer la conversión de celsius a fahrenheit presione digite el número 1 <br> si desea hacer la conversión de fahrenheit a celsius digite el número 2"))
+let grados = parseInt(prompt("- Si desea hacer la conversión de celsius a fahrenheit presione digite el número 1 \n - Si desea hacer la conversión de fahrenheit a celsius digite el número 2"))
 
 
 const temperatura = (param1, param2) => {
@@ -19,23 +19,23 @@ const temperatura = (param1, param2) => {
 }
 
 const validacion = (otraFuncion) => {
-    let info = "El numero ingresado por usted es: " + numero + "<br> el resultado de la conversión es: " + otraFuncion(grados,numero)
+    let info = "El numero ingresado por usted es: " + numero + "<br> el resultado de la conversión es: " + otraFuncion
         if(grados == 2){
-            if(otraFuncion(grados,numero) < 0){
+            if(otraFuncion < 0){
                 return info + " Nos congelamos <br>"
-            }else if(otraFuncion(grados,numero) < 16){
+            }else if(otraFuncion < 16){
                 return info + " Está haciendo frío <br>"
-            }else if (otraFuncion(grados,numero) < 25){
+            }else if (otraFuncion < 25){
                 return info + " Está templado el día <br>"
             }else {
                 return info + " Tiene friebre o el apocalipsis <br>"
             }
         } else if(grados == 1){
-            if(otraFuncion(grados,numero) < 32){
+            if(otraFuncion < 32){
                 return info + " Nos congelamos <br>"
-            }else if(otraFuncion(grados,numero) < 50){
+            }else if(otraFuncion < 50){
                 return info + " Está haciendo frío <br>"
-            }else if (otraFuncion(grados,numero) < 77){
+            }else if (otraFuncion  < 77){
                 return info + " Está templado el día <br>"
             }else {
                 return info + " Tiene friebre o el apocalipsis <br>"
@@ -43,4 +43,4 @@ const validacion = (otraFuncion) => {
         }
     
 }
-document.write(validacion(temperatura))
+document.write(validacion(temperatura(grados,numero)))
